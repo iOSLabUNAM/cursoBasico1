4b333268dd53e1c62d3662e5d4732539a911d8e9
Pedro Vazquez <pedrovazquezax@gmail.com>
2020-01-08 09:47:41 -0600

    proyecto ahorcado UI

---
+++ 08:01:2020/ahorcado/ahorcado/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ahorcado
+//
+//  Created by Pedro Antonio Vazquez Rodriguez on 08/01/20.
+//  Copyright © 2020 Pedro Antonio Vazquez Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
